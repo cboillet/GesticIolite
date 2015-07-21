dbb87f8a0f9d2df4212e3d6ba5cbd999238ac30f
cboillet <camille.boillet@gmail.com>
2015-07-21 23:05:16 +0200

    listener lvl 1

--- src/com/gesticiolite/view/MenuActivity.java
+++ src/com/gesticiolite/view/MenuActivity.java
@@ -5,12 +5,20 @@ import java.util.List;
 import com.gesticiolite.R;
 import com.gesticiolite.gestures.*;
 import com.gesticiolite.gestures.classifier.Distribution;
-
 import com.gesticiolite.gestures.IGestureRecognitionListener;
+
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -28,13 +36,94 @@ import android.os.RemoteException;
 
 public class MenuActivity extends Activity{
 
+	String trainingSet;
+	private IGestureRecognitionService recognitionService;
+	private final ServiceConnection serviceConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			recognitionService = IGestureRecognitionService.Stub.asInterface(service);
+		      try {     
+		    	    recognitionService.startClassificationMode("default");
+		    	  	recognitionService.registerListener(IGestureRecognitionListener.Stub.asInterface(gestureListenerStub));
+			      } catch (RemoteException e) {
+			         e.printStackTrace();
+			      }
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName className) {
+			recognitionService = null;
+		}
+	};
+	
+	IBinder gestureListenerStub = new IGestureRecognitionListener.Stub() {
+		@Override
+		public void onGestureRecognized(final Distribution distribution) throws RemoteException {
+			System.out.println("gesture recognized");
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Toast.makeText(MenuActivity.this, String.format("%s: %f", distribution.getBestMatch(), distribution.getBestDistance()), Toast.LENGTH_LONG).show();
+					System.err.println(String.format("%s: %f", distribution.getBestMatch(), distribution.getBestDistance()));
+				}
+			});
+		}
+
+		@Override
+		public void onGestureLearned(String gestureName) throws RemoteException {
+		}
+
+		@Override
+		public void onTrainingSetDeleted(String trainingSet)
+				throws RemoteException {}
+	};
+	
+	@Override
+	protected void onResume() {
+		trainingSet = getIntent().getStringExtra("trainingSetName");
+		Intent bindIntent = new Intent(this, GestureRecognitionService.class);
+		bindService(bindIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		try {
+			recognitionService.unregisterListener(IGestureRecognitionListener.Stub.asInterface(gestureListenerStub));
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		recognitionService = null;
+		unbindService(serviceConnection);
+		super.onPause();
+	}
+	
+	
 	@Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.main_menu);
+	        setContentView(R.layout.main_menu);        
+	        trainingSet = getIntent().getStringExtra("trainingSetName");
 			
-//	        Intent bindIntent = new Intent("com.gesticiolite.gestures.GESTURE_RECOGNIZER");
-//			bindService(bindIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+	        final Button start = (Button) findViewById(R.id.startClassifying);
+
+			start.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+						if (recognitionService != null) {
+						try {
+							recognitionService.startClassificationMode(trainingSet);
+							System.out.println("starting classifying");
+						} catch (RemoteException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			});
 	        
 	        // 1. get a reference to recyclerView 
 	        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
@@ -56,10 +145,5 @@ public class MenuActivity extends Activity{
 	        recyclerView.setItemAnimator(new DefaultItemAnimator());
 	    }
 	
-		@Override
-		protected void onResume() {}
-		
-		
-		@Override
-		protected void onPause() {}
+
 };
